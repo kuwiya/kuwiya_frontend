1ff546705ab482a8a85aba4ad6766cc5bba00f01
encodedevv <timothyonyea@gmail.com>
2024-04-18 00:37:38 +0100

    fix to marketplace

--- src/pages/marketplace/_components/AffordableYummy.jsx
+++ src/pages/marketplace/_components/AffordableYummy.jsx
@@ -4,7 +4,7 @@ const AffordableYummy = ({ image }) => {
   return (
     <section className="mx-6 md:mx-16 lg:mx-[136px] my-16 relative font-lato font-bold text-base md:text-2xl lg:text-5xl h-fit">
       <div className="flex h-fit">
-        <div className="flex-[67%] py-6 md:py-0 rounded-l-[10px] text-center flex flex-col justify-center items-center lg:gap-3 text-white bg-gradient-to-r from-[#F29309] from-[0%] via-[14%] via-[#8D5D1E] to-[#000000] to-[50%]">
+        <div className="py-6 md:py-0 rounded-l-[10px] text-center flex flex-col justify-center items-center lg:gap-3 text-white bg-gradient-to-r from-[#F29309] from-[0%] via-[14%] via-[#8D5D1E] to-[#000000] to-[50%]">
           <h1 className="">Affordable yummy tasty</h1>
           <h1 className="lg:px-20 md:px-20 px-9 lg:leading-[60px]">
             Advertise here to reach 2 million viewers
