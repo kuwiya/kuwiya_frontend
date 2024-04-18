581fe9bf2c13cfea58e64a960e047608d5ccdb5c
encodedevv <timothyonyea@gmail.com>
2024-04-18 01:06:02 +0100

    width fix on marketplace

--- src/pages/marketplace/_components/AffordableYummy.jsx
+++ src/pages/marketplace/_components/AffordableYummy.jsx
@@ -5,16 +5,19 @@ const AffordableYummy = ({ image }) => {
     <section className="mx-6 md:mx-16 lg:mx-[136px] my-16 relative font-lato font-bold text-base md:text-2xl lg:text-5xl">
       <div className="flex">
         <div className="md:py-0 rounded-l-[10px] text-center flex flex-col justify-center items-center lg:gap-3 text-white bg-gradient-to-r from-[#F29309] from-[0%] via-[14%] via-[#8D5D1E] to-[#000000] to-[50%]">
-          <h1 className="">Affordable yummy tasty</h1>
-          <h1 className="lg:px-20 md:px-20 px-9 lg:leading-[60px]">
-            Advertise here to reach 2 million viewers
-          </h1>
+          <div>
+          Affordable yummy tasty Advertise here to reach 2 million viewers
+          </div>
+          {/* <h1 className=""></h1> */}
+          {/* <h1 className="lg:px-20 md:px-20 px-9 lg:leading-[60px]">
+            
+          </h1> */}
         </div>
         <div className="">
           <img
             src={image}
             alt=""
-            className="h-full rounded-r-[10px] object-contain"
+            className=" rounded-r-[10px] object-contain"
           />
         </div>
       </div>
