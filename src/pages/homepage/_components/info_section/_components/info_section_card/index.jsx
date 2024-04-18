0ed3ccf0430301cb455dacac0f800775feff3ff1
encodedevv <timothyonyea@gmail.com>
2024-04-18 19:45:38 +0100

    explore cards resizing

--- src/pages/homepage/_components/info_section/_components/info_section_card/index.jsx
+++ src/pages/homepage/_components/info_section/_components/info_section_card/index.jsx
@@ -6,7 +6,7 @@ import { Button } from "../../../../../../components/ui";
 const InfoSectionCard = () => {
   return (
     <div className="mx-6 md:mx-10 py-3">
-      <div className="flex md:flex-row pl-[4rem] md:pl-[32rem] lg:p-0 no-scrollbar py-4 overflow-x-scroll lg:overflow-hidden md:justify-around items-center gap-12 md:gap-12 md:py-6">
+      <div className="flex md:flex-row pl-[2rem] md:pl-[32rem] lg:p-0 no-scrollbar py-4 overflow-x-scroll lg:overflow-hidden md:justify-around items-center gap-12 md:gap-12 md:py-6 w-[107%] md:w-full">
         {InfoDetails.map((info) => (
           <div
             className="flex items-center flex-col min-w-[80%] md:min-w-[330px] lg:min-w-[380px]  p-5 lg:p-10 shadow-xl md:shadow-lg hover:scale-105 hover:cursor-pointer transition-all ease-in-out lg:w-[380px] md:h-[450px] rounded-[20px] gap-10"
