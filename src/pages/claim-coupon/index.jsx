7d7e09eecf585db1e231cad0d06ca2f7bec8efe4
Israel Collins, Adigun <163291957+Israel-tobamsgroup@users.noreply.github.com>
2024-04-21 15:53:00 +0100

    claim-coupon

--- src/pages/claim-coupon/index.jsx
+++ src/pages/claim-coupon/index.jsx
@@ -17,11 +17,15 @@ const ClaimCoupon = () => {
   const { isLoading, data, isError, error } = useClaimCoupon(id);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="font-semibold text-2xl text-center">Loading...</div>;
   }
 
   if (isError) {
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className="font-semibold text-2xl text-center">
+        Error: {error.message}
+      </div>
+    );
   }
 
   const coupon = data?.data;
@@ -29,20 +33,20 @@ const ClaimCoupon = () => {
   return (
     <>
       <Navbar scrolling />
-      <main className="md:pt-40 pt-36 px-6 md:px-20">
-      <div className="lg:px-20 ">
-        <div className="flex gap-2 items-center font-medium lg:text-[23px]">
-          <Link to="/">
-            <ArrowLeft />
-          </Link>
-          <p>Coupon Item Details</p>
+      <main className="md:pt-32 pt-20 px-6 md:px-20">
+        <div className="lg:px-20 ">
+          <div className="flex gap-2 items-center font-medium lg:text-[23px]">
+            <Link to="/">
+              <ArrowLeft />
+            </Link>
+            <p>Coupon Item Details</p>
+          </div>
         </div>
-      </div>
 
-      <section className="py-7 space-y-5 lg:px-20">
+        <section className="py-7 space-y-5 lg:px-20">
           <div className="flex relative">
             <div className="w-7 h-7 md:w-12 md:h-12 rounded-full bg-white absolute top-[50%] -left-4 md:-left-5 -translate-y-[50%]"></div>
-            <div className="flex bg-gradient-to-r from-[#000000] from-[0%] to-[#666666] to-[100%] rounded-lg p-1 md:p-2">
+            {/* <div className="flex bg-gradient-to-r from-[#000000] from-[0%] to-[#666666] to-[100%] rounded-lg p-1 md:p-2">
               <div className="md:flex-[55%] lg:pt-12 md:pt-8 pt-2 md:pb-6 lg:pl-16 md:pl-10 pl-7 text-[#000000]">
                 <h1 className="lg:text-5xl md:text-[40px] font-bold text-white -mb-4 md:mb-0">
                   {coupon.mealName}
@@ -120,6 +124,85 @@ const ClaimCoupon = () => {
                   </span>
                 </div>
               </div>
+            </div> */}
+            <div className="flex bg-gradient-to-r from-[#000000] from-[0%] to-[#666666] to-[100%] rounded-lg p-1 md:p-2">
+              <div className="flex-1 md:flex-[55%] h-[169px] md:h-auto lg:pt-12 md:pt-8 pt-2 md:pb-6 lg:pl-16 md:pl-10 pl-7 text-[#000000]">
+                <h1 className="lg:text-5xl md:text-[40px] font-bold text-white -mb-4 md:mb-0">
+                  {coupon.mealName}
+                </h1>
+                <h1 className="lg:text-8xl md:text-[64px] text-2xl font-bold text-darkyellow mt-3">
+                  {coupon.priceOff} OFF
+                </h1>
+                <p className="lg:text-[28px] md:text-[19px] text-[11px] font-normal text-[#FFFFFFDE] md:mt-4 lg:mt-0">
+                  Copy Code Below to claim Coupons
+                </p>
+                {isCopied && (
+                  <p className="text-green-600 font-medium md:text-xl text-[11px]">
+                    Coupon Code Copied!
+                  </p>
+                )}
+                <div
+                  onClick={() => {
+                    navigator.clipboard.writeText(
+                      `${coupon.couponNum}-${coupon.couponCode}`
+                    );
+                    setIsCopied(true);
+                    setTimeout(() => {
+                      setIsCopied(false);
+                    }, 4000);
+                  }}
+                  className="w-fit border-[1px] group flex items-center cursor-pointer hover:text-darkyellow hover:border-darkyellow border-white mt-1 md:mt-4 rounded-[10px] lg:px-16 md:px-8 px-2.5 py-[2px] md:py-2 border-dashed text-white md:font-semibold font-medium lg:text-[40px] md:text-[30px] text-lg"
+                >
+                  <h1 className="">{coupon.couponNum}</h1>
+                  <span className="bg-[#FFFFFF] h-0.5 w-2 group-hover:bg-darkyellow"></span>
+                  <h1 className="">{coupon.couponCode}</h1>
+                </div>
+                <div className="flex gap-3 md:gap-3 items-center mt-1 md:mt-5 lg:text-base md:text-sm text-[8px] font-normal text-[#FFFFFFBF]">
+                  <p>{coupon.restName}</p>
+                  <span className="h-4 w-[1px] bg-[#FFFFFFBF]"></span>
+                  <p className="flex gap-[3px] items-center justify-between">
+                    <span className="hidden md:block">
+                      <Clock fillColor={"#FFFFFFBF"} />
+                    </span>
+                    <span className="md:hidden">
+                      <Clock fillColor={"#FFFFFFBF"} width={10} height={10} />
+                    </span>
+                    <span>{coupon.duration}</span>
+                  </p>
+                  <span className="hidden md:block h-4 w-[1px] bg-[#FFFFFFBF]"></span>
+                  <p className="hidden md:flex gap-[3px] items-center">
+                    <span className="hidden md:block">
+                      <Star fillColor={"#FFFFFFBF"} />
+                    </span>
+                    <span className="md:hidden">
+                      <Star fillColor={"#FFFFFFBF"} width={10} height={10} />
+                    </span>
+                    <span>{coupon.rating}</span>
+                    <span>({coupon.rating_count})</span>
+                  </p>
+                </div>
+              </div>
+              <div className="flex-1 md:flex-[45%] flex md:gap-1 items-center">
+                <div className="hidden lg:block">
+                  <CouponText />
+                </div>
+                <div className="hidden md:block lg:hidden">
+                  <CouponText width={75} height={240} />
+                </div>
+                <div className="md:hidden">
+                  <CouponText width={38.1} height={120} />
+                </div>
+                <div className="relative h-[161px] md:h-full flex-[70%]">
+                  <img
+                    src={coupon.mealImage}
+                    className="rounded-lg h-full object-cover"
+                    alt=""
+                  />
+                  <span className="absolute top-2 right-2 md:top-5 md:right-5 z-10 p-[3px] md:p-[5px] bg-white rounded-full w-8 h-8 md:w-10 md:h-10 flex items-center justify-center">
+                    <img src={coupon.restLogo} alt="" />
+                  </span>
+                </div>
+              </div>
             </div>
             <div className="w-7 h-7 md:w-12 md:h-12 rounded-full bg-white absolute top-[50%] -right-4 md:-right-5 -translate-y-[50%]"></div>
           </div>
@@ -171,7 +254,7 @@ const ClaimCoupon = () => {
             </p>
           </div>
         </section>
-      <section className="space-y-3 mt-7 lg:px-20 mb-24">
+        <section className="space-y-3 mt-7 lg:px-20 mb-24">
           <p className="font-normal lg:text-[19px] lg:pl-9">Similar Coupon</p>
           <ClaimCouponFiltering restName={coupon.restName} id={coupon.id} />
         </section>
