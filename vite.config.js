be9f6a77d99c14f648ce05e8ebf4a9b7c24e6ef9
Adigun Israel Collins <adigunisrael@gmail.com>
2024-05-13 15:03:10 +0100

    server host change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // port: true,
-    host: "172.31.24.17",
+    host: true,
+    // port: 5173,
   },
 });
